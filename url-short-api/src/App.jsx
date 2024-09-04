e6d92821f532107a30393dca3a84a7fa8c2552e4
Fernanda <pazocampo.fer@gmail.com>
2024-09-03 22:48:47 -0700

    Adding Main component to App file, to work on further components on Main

--- url-short-api/src/App.jsx
+++ url-short-api/src/App.jsx
@@ -2,13 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import React from "react";
 import Header from "../src/components/header/Header";
+import Main from "../src/components/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Main />
-      <Footer /> */}
+      <Main />
+      {/* <Footer /> */}
     </div>
   );
 }
