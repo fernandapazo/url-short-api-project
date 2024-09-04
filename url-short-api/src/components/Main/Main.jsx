5fe305878ad992382ab6d1f984f52578a4a7435e
Fernanda <pazocampo.fer@gmail.com>
2024-09-03 22:51:32 -0700

    Importing Hero component onto Main component

---
+++ url-short-api/src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../Hero/Hero";
+
+function Main() {
+  return (
+    <main>
+      <div>
+        <div>
+          <Hero />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
