e9a784440a0918bfdc8488fe94e2ff8b590e82ce
Fernanda <pazocampo.fer@gmail.com>
2024-09-03 22:50:52 -0700

    Keeping Hero simple

---
+++ url-short-api/src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Hero.scss";
+
+function Hero() {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center min-vh-100 text-center">
+      <h1 className="fs-1 fw-bold">Shorten your links with ease</h1>
+      <span>
+        Simplify your online presence with our powerful link shortening tool.
+      </span>
+    </div>
+  );
+}
+
+export default Hero;
